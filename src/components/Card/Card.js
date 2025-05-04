f46db7efd98c1755fcbc27e61fb12caa0d28507a
Calvin <calvinandrean456@gmail.com>
2025-05-04 11:24:49 +0700

    add spk and satu sehat portfolio

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React from "react";
 
-function Card({
-  title,
-  description,
-  image,
-  tech_stack,
-  link // Add a link prop for the target URL
-}) {
-  return (
-    <a href={link} target="_blank" rel="noopener noreferrer" className='flex flex-col justify-center items-center'>
-      <div className='group relative m-4 p-4 bg-white hover:bg-opacity-60 backdrop-filter backdrop-blur-lg rounded-md shadow-md w-[400px] lg:w-[600px] transform transition-all duration-300 hover:scale-105 cursor-pointer'>
-        <img src={image ? image : 'https://robohash.org/200'} alt='project' className='w-full h-80 object-contain rounded-md' />
-        <h1 className='text-[#393b2e] font-bold text-2xl mt-2'>{title ? title : 'Title'}</h1>
-        <p className='text-[#393b2e]'>{description ? description : 'Lorem ipsum'}</p>
-        <div className='flex justify-between items-center mt-6'>
-          <div className='flex flex-wrap gap-2 w-3/4'>
-            {tech_stack.map((tech, index) => (
-              <span key={index} className='bg-[#393b2e] text-white py-1 px-2 rounded-md'>{tech}</span>
-            ))}
-          </div>
-          <button className='hidden group-hover:flex bg-transparent border border-[#393b2e] text-[#393b2e] py-1 px-4 rounded-md hover:bg-[#393b2e] hover:text-white transition-all duration-300'>{'->'}</button>
+function Card({ title, description, image, tech_stack, link }) {
+  const isValidLink = link && link !== "null";
+
+  const content = (
+    <div className="group relative m-4 p-4 bg-white hover:bg-opacity-60 backdrop-filter backdrop-blur-lg rounded-md shadow-md w-[400px] lg:w-[600px] transform transition-all duration-300 hover:scale-105 cursor-pointer">
+      <img
+        src={image ? image : "https://robohash.org/200"}
+        alt="project"
+        className="w-full h-80 object-contain rounded-md"
+      />
+      <h1 className="text-[#393b2e] font-bold text-2xl mt-2">
+        {title || "Title"}
+      </h1>
+      <p className="text-[#393b2e]">{description || "Lorem ipsum"}</p>
+      <div className="flex justify-between items-center mt-6">
+        <div className="flex flex-wrap gap-2 w-3/4">
+          {tech_stack.map((tech, index) => (
+            <span
+              key={index}
+              className="bg-[#393b2e] text-white py-1 px-2 rounded-md"
+            >
+              {tech}
+            </span>
+          ))}
         </div>
+        <button className="hidden group-hover:flex bg-transparent border border-[#393b2e] text-[#393b2e] py-1 px-4 rounded-md hover:bg-[#393b2e] hover:text-white transition-all duration-300">
+          {"->"}
+        </button>
       </div>
+    </div>
+  );
+
+  return isValidLink ? (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex flex-col justify-center items-center"
+    >
+      {content}
     </a>
+  ) : (
+    <div className="flex flex-col justify-center items-center cursor-default">
+      {content}
+    </div>
   );
 }
 
--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,35 +1,64 @@
 const projectData = [
   {
     id: 1,
-    title: 'AlumniTreasures',
-    description: 'AlumniTreasures is an app that helps communities share unused items with features like live chat messaging, geolocation, and secure OTP login. The app promotes sharing and community connections, and is available for both Android and iOS.',
-    image: './assets/images/web-hero2.png',
-    tech_stack: ['React Native', 'Express.js', 'Node.js', 'MySQL', 'Android', 'iOS'],
-    link: 'https://alumnitreasures-promo.vercel.app'
+    title: "AlumniTreasures",
+    description:
+      "AlumniTreasures is an app that helps communities share unused items with features like live chat messaging, geolocation, and secure OTP login. The app promotes sharing and community connections, and is available for both Android and iOS.",
+    image: "./assets/images/web-hero2.png",
+    tech_stack: [
+      "React Native",
+      "Express.js",
+      "Node.js",
+      "MySQL",
+      "Android",
+      "iOS",
+    ],
+    link: "https://alumnitreasures-promo.vercel.app",
   },
   {
     id: 2,
-    title: 'KKN Pringgading',
-    description: 'The website for Padukuhan Pringgading provides information about the community located in Kalurahan Guwosari, Bantul. It highlights the history, key landmarks, and the local environment, promoting a deeper connection with Pringgading.',
-    image: './assets/images/kkn-pringgading.jpeg',
-    tech_stack: ['ReactJS', 'NextJS', 'TailwindCSS', 'Figma'],
-    link: 'https://kkn-pringgading.vercel.app'
+    title: "KKN Pringgading",
+    description:
+      "The website for Padukuhan Pringgading provides information about the community located in Kalurahan Guwosari, Bantul. It highlights the history, key landmarks, and the local environment, promoting a deeper connection with Pringgading.",
+    image: "./assets/images/kkn-pringgading.jpeg",
+    tech_stack: ["ReactJS", "NextJS", "TailwindCSS", "Figma"],
+    link: "https://kkn-pringgading.vercel.app",
   },
   {
     id: 3,
-    title: 'AtmaKitchen',
-    description: 'Kitchen management app that helps staff to manage orders, inventory, and customer data. The app is designed to be user-friendly and efficient, with features like katalog, order tracking, and real-time notifications.',
-    image: './assets/images/showcase-atmakitchen.png',
-    tech_stack: ['React Native', 'Express.js', 'Node.js', 'MySQL', 'Android'],
-    link: 'null'
+    title: "AtmaKitchen",
+    description:
+      "Kitchen management app that helps staff to manage orders, inventory, and customer data. The app is designed to be user-friendly and efficient, with features like katalog, order tracking, and real-time notifications.",
+    image: "./assets/images/showcase-atmakitchen.png",
+    tech_stack: ["React Native", "Express.js", "Node.js", "MySQL", "Android"],
+    link: "null",
   },
   {
     id: 4,
-    title: 'GoFit',
-    description: 'GoFIt is a fitness app that lets users book and manage fitness classes, view daily schedules, and access membership information. It provides an organized interface for handling fitness activities, class deposits, and personal profiles.',
-    image: './assets/images/gofit.png',
-    tech_stack: ['Flutter', 'Android', 'Java', 'MySQL', 'Laravel'],
-    link: 'null'
+    title: "GoFit",
+    description:
+      "GoFIt is a fitness app that lets users book and manage fitness classes, view daily schedules, and access membership information. It provides an organized interface for handling fitness activities, class deposits, and personal profiles.",
+    image: "./assets/images/gofit.png",
+    tech_stack: ["Flutter", "Android", "Java", "MySQL", "Laravel"],
+    link: "null",
+  },
+  {
+    id: 5,
+    title: "Satu Sehat",
+    description:
+      "A blockchain-based platform designed to enable secure, verified, and efficient sharing of medical data among hospitals, clinics, community health centers, government institutions, and patients; ensuring transparency, data integrity, and improved collaboration across the healthcare ecosystem.",
+    image: "./assets/images/cover-satu-sehat.png",
+    tech_stack: ["ReactJS", "NextJS", "TailwindCSS", "Multichain"],
+    link: "https://82.112.230.186:3000",
+  },
+  {
+    id: 6,
+    title: "Sistem Pendukung Keputusan: AHP & Maturity",
+    description:
+      "A website designed to assess the risk level of a project within a company, enabling the implementation of effective and efficient risk management strategies for both prevention and mitigation.",
+    image: "./assets/images/spk.png",
+    tech_stack: ["ReactJS", "NextJS", "TailwindCSS"],
+    link: "null",
   },
 ];
 
